93e42828b0633d60d4705b54cbac8a8938a62b3b
Mike McNeeley <mmcneeley@gmail.com>
2018-03-01 01:11:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
     line_string = "The line is currently:"
     katz_deli.each do |person, x|
       x += 1
-      line_string = line_string + " #{x}. #{person}"
+      line_string = "#{line_string} #{x}. #{person}"
     end
     puts line_string
   end
