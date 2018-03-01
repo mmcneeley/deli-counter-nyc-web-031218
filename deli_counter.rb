787e05ef585a0b4252a1608040c46710a3c75ce4
Mike McNeeley <mmcneeley@gmail.com>
2018-03-01 01:06:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,14 +11,14 @@ def line(katz_deli)
       line_string = line_string + " #{x}. #{person}"
     end
   end
-  line_string
+  puts line_string
 end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 def now_serving(katz_deli)
-  "Currently serving #{katz_deli.shift}."
+  puts "Currently serving #{katz_deli.shift}."
 end
